d104362c5bfe3e4b403f882021b1261dc9ec7207
crsbo <202401983@pua.edu.eg>
2025-04-08 18:04:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
 import json
 import os
+import validators  # لتحقق من صحة الروابط
 
 app = Flask(__name__)
 DATA_FILE = 'links_data.json'
 
+# إنشاء الملف إذا كان غير موجود
 if not os.path.exists(DATA_FILE):
     with open(DATA_FILE, 'w') as f:
         json.dump({}, f)
@@ -13,6 +15,7 @@ if not os.path.exists(DATA_FILE):
 def home():
     return render_template('index.html')
 
+# صفحة إضافة اللينكات
 @app.route('/add_links', methods=['POST'])
 def add_links():
     username = request.form['username']
@@ -29,12 +32,18 @@ def user_links(username):
     if request.method == 'POST':
         title = request.form['title']
         url = request.form['url']
-        data[username].append((title, url))
+
+        # تحقق من صحة الرابط
+        if not validators.url(url):
+            return "Invalid URL", 400
+
+        data[username].append({'title': title, 'url': url})
         with open(DATA_FILE, 'w') as f:
             json.dump(data, f)
 
     return render_template('links.html', username=username, links=data[username])
 
+# حذف لينك
 @app.route('/delete_link/<username>/<int:index>')
 def delete_link(username, index):
     with open(DATA_FILE, 'r') as f:
@@ -48,6 +57,7 @@ def delete_link(username, index):
 
     return redirect(url_for('user_links', username=username))
 
+# صفحة البايو التي تعرض اللينكات
 @app.route('/bio/<username>')
 def show_bio(username):
     with open(DATA_FILE, 'r') as f:
