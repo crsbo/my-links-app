a30887be22e03c198cd60ea0f1d95aeca5e66342
crsbo <202401983@pua.edu.eg>
2025-04-08 19:35:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -41,21 +41,4 @@ def user_links(username):
 @app.route('/confirm_links/<username>', methods=['POST'])
 def confirm_links(username):
     with open(DATA_FILE, 'r') as f:
-        data = json.load(f)
-
-    bio_url = url_for('show_bio', username=username, _external=True)
-    return render_template('bio.html', username=username, links=data[username], bio_url=bio_url)
-
-# صفحة عرض البروفايل مع اللينكات
-@app.route('/bio/<username>')
-def show_bio(username):
-    with open(DATA_FILE, 'r') as f:
-        data = json.load(f)
-
-    user_links = data.get(username, [])
-    bio_url = url_for('show_bio', username=username, _external=True)
-    return render_template('bio.html', username=username, links=user_links, bio_url=bio_url)
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+        data = json.load(f
