b9f24593865c05a39f3c14d8a8d5c93710888c7f
crsbo <202401983@pua.edu.eg>
2025-04-08 17:49:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import json
 import os
 
@@ -13,23 +13,40 @@ if not os.path.exists(DATA_FILE):
 def home():
     return render_template('index.html')
 
-@app.route('/submit', methods=['POST'])
-def submit():
+@app.route('/add_links', methods=['POST'])
+def add_links():
     username = request.form['username']
-    link = request.form['link']
+    return redirect(url_for('user_links', username=username))
 
+@app.route('/add_links/<username>', methods=['GET', 'POST'])
+def user_links(username):
     with open(DATA_FILE, 'r') as f:
         data = json.load(f)
 
     if username not in data:
         data[username] = []
 
-    data[username].append(link)
+    if request.method == 'POST':
+        title = request.form['title']
+        url = request.form['url']
+        data[username].append((title, url))
+        with open(DATA_FILE, 'w') as f:
+            json.dump(data, f)
+
+    return render_template('links.html', username=username, links=data[username])
+
+@app.route('/delete_link/<username>/<int:index>')
+def delete_link(username, index):
+    with open(DATA_FILE, 'r') as f:
+        data = json.load(f)
+
+    if username in data and 0 <= index < len(data[username]):
+        data[username].pop(index)
 
     with open(DATA_FILE, 'w') as f:
         json.dump(data, f)
 
-    return redirect(f'/bio/{username}')
+    return redirect(url_for('user_links', username=username))
 
 @app.route('/bio/<username>')
 def show_bio(username):
@@ -37,10 +54,8 @@ def show_bio(username):
         data = json.load(f)
 
     user_links = data.get(username, [])
-
     return render_template('bio.html', username=username, links=user_links)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
